6073962c2b898dd172e9982a69fb87aff63db5c4
Gabriela Benavides <gabrielarussell@my.whccd.rdu>
2024-01-28 22:42:20 -0800

    adding things

---
+++ COMP003A.Assignment2/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Author: Gabriela Benavides
+ * Course: Comp-003A.Assignment2
+ * Purpose:Programming Basics
+ */
+using System.Diagnostics;
+
+namespace COMP003A.Assignment2
+{
+
+    internal class Program
+ {
+     static void Main(string[] args)
+     {
+         Console.BackgroundColor = ConsoleColor.DarkBlue;
+         Console.ForegroundColor = ConsoleColor.White;
+
+         string firstName = Console.ReadLine(Gabiela);
+         string middleName = Console.ReadLine(Nicole);
+         string lastName = Console.ReadLine(Benavides);
+         string inputAge = Console.ReadLine(36);
+         
+     }
+    
+
+ 
